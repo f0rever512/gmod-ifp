29461791d79aa815dc0601667c9e9af560c171ed
f0rever512 <103198529+f0rever512@users.noreply.github.com>
2026-03-28 16:19:09 +0700

    💄 Shader warning menu updated 💄 Меню предупреждения шейдера обновлено

--- lua/ifp/cl_modifiers.lua
+++ lua/ifp/cl_modifiers.lua
@@ -1,6 +1,46 @@
-local ifpTable = ifpTable
+local vgui = vgui
+local language = language
+local render = render
+
+local cv_shaderWarn = CreateClientConVar('cl_ifp_shader_warn', '1')
 
 local shaderWarn = false
+
+local function shaderWarnMenu()
+
+	local f = vgui.Create('DFrame')
+	f:SetTitle(language.GetPhrase('gmod_ifp.ui.shader_warn.title'))
+	f:SetSize(460, 180)
+	f:Center()
+	f:MakePopup()
+
+	local l = vgui.Create('DLabel', f)
+	l:SetText(language.GetPhrase('gmod_ifp.ui.shader_warn.text'))
+	l:SizeToContents()
+	l:Dock(FILL)
+	l:DockMargin(8, 4, 8, 4)
+
+	local c = vgui.Create('DCheckBoxLabel', f)
+	c:SetText(language.GetPhrase('gmod_ifp.ui.shader_warn.donotshow'))
+	c:SizeToContents()
+	c:Dock(BOTTOM)
+	c:DockMargin(8, 8, 0, 8)
+	c:SetChecked(not cv_shaderWarn:GetBool())
+	function c:OnChange(val)
+		RunConsoleCommand(cv_shaderWarn:GetName(), val and '0' or '1')
+	end
+
+	local b = vgui.Create('DButton', f)
+	b:SetText(language.GetPhrase('gmod_ifp.ui.shader_warn.link'))
+	b:Dock(BOTTOM)
+	b:DockMargin(4, 0, 4, 0)
+	b:SetTall(28)
+	b:SetIcon('icon16/link.png')
+	function b:DoClick()
+		gui.OpenURL('https://steamcommunity.com/sharedfiles/filedetails/?id=3440271589')
+	end
+
+end
 
 ifpTable.mods = {
 	{
@@ -13,28 +53,12 @@ ifpTable.mods = {
 		znear = 1,
 
 		shaderFunc = function()
+
+			-- simple custom shaders check
 			if not render.DrawMercFisheye then
 				if not shaderWarn then
-					local frame = vgui.Create('DFrame')
-					frame:SetTitle(language.GetPhrase('gmod_ifp.ui.shader_warn.title'))
-					frame:SetSize(400, 160)
-					frame:Center()
-					frame:MakePopup()
-
-					local label = vgui.Create('DLabel', frame)
-					label:SetText(language.GetPhrase('gmod_ifp.ui.shader_warn.text'))
-					label:SizeToContents()
-					label:Dock(FILL)
-					label:DockMargin(8, 4, 8, 4)
-
-					local linkBtn = vgui.Create('DButton', frame)
-					linkBtn:SetText(language.GetPhrase('gmod_ifp.ui.shader_warn.link'))
-					linkBtn:Dock(BOTTOM)
-					linkBtn:DockMargin(4, 4, 4, 4)
-					linkBtn:SetTall(32)
-					linkBtn:SetIcon('icon16/link.png')
-					function linkBtn:DoClick()
-						gui.OpenURL('https://steamcommunity.com/sharedfiles/filedetails/?id=3440271589')
+					if cv_shaderWarn:GetBool() then
+						shaderWarnMenu()
 					end
 
 					shaderWarn = true
@@ -46,6 +70,7 @@ ifpTable.mods = {
 			-- shaders from https://steamcommunity.com/sharedfiles/filedetails/?id=3440271589
 			render.DrawMercFisheye(0.2)
 			render.DrawMercVignette(0.9, 0.5)
+
 		end,
 	},
 }
